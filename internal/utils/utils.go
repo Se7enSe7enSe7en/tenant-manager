3cec8c0529280d45aa597507acf3e5d533a0dd09
Jedden Mendoza <jedden.mendoza@gmail.com>
2026-03-05 23:33:51 +0800

    build: project restructure

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,6 +2,11 @@ package utils
 
 import "strings"
 
+/*
+This function is for turning any value into a pointer,
+our main usecase for this as of the moment is to handle optional
+or nullable behavior for the front end.
+*/
 func Ptr[T any](v T) *T {
 	return &v
 }
